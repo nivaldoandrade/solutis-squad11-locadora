a96fb8de3b4612bbb524096ad628605d25ecb220
nivaldoandrade <nivaldoandradef@gmail.com>
2024-09-02 17:57:10 -0300

    fix: enforce strict date parsing with ResolverStyle.STRICT in DateTimeFormatter

--- src/main/java/com/squad11/locadora/utils/DateUtils.java
+++ src/main/java/com/squad11/locadora/utils/DateUtils.java
@@ -2,9 +2,10 @@ package com.squad11.locadora.utils;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
 
 public class DateUtils {
-    public static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
 
     public static LocalDate formatStringToDate(String date) {
         return LocalDate.parse(date, DTF);
